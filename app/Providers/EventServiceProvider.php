0122446e44fa20247f74112062253079eb82953d
Ramy <115069282+ramy-abbas@users.noreply.github.com>
2024-02-29 14:21:08 +0200

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,10 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
+
+
+
+    
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
